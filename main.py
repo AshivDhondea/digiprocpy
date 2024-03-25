3ebcc293137e8fa58a2811e74e8bd7ebb2a1deea
Hans Dhondea <ashivdhondea5@gmail.com>
2024-03-25 11:29:09 -0700

    Initial work. Added initial work for digiprocpy

---
+++ main.py
@@ -0,0 +1,6 @@
+from src.utils import linear_algebra_functions as lin_alg
+
+
+if __name__ == '__main__':
+    print("general library to run computational/numerical calculations, signal, image and data processing tasks.")
+    
